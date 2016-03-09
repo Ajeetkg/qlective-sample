6666ae3da0c2bc6b9c450d8259bca0ff78ba2554
Ajeet Gupta <agupta2@illumina.com>
2016-03-09 11:02:12 -0800

    Added code to store the psToken and expire it afte 10 seconds

--- src/main/java/com/ajeetkg/service/impl/HelloServiceImpl.java
+++ src/main/java/com/ajeetkg/service/impl/HelloServiceImpl.java
@@ -6,7 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.stereotype.Service;
+
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by agupta2 on 3/8/16.
@@ -21,29 +24,33 @@ public class HelloServiceImpl implements HelloService{
     @Autowired
     private CacheManager cacheManager;
 
+
+    @Autowired
+    RedisTemplate<String, String> redisTemplate;
+
+
     @Override
     @Cacheable(cacheNames = "messageCache")
     public String getMessage(String name) {
-
-
+        delay();
+        storePsToken("Illumina");
         logger.debug("cachemangagerName:" + cacheManager.getCacheNames());
         System.out.println("cacheManager.getCacheNames():   " + cacheManager.getCacheNames());
-        System.out.println("Executing HelloServiceImpl" +
-                ".getHelloMessage(\"" + name + "\")");
-
+        System.out.println("Executing HelloServiceImpl" + ".getHelloMessage(\"" + name + "\")");
         return "Hello " + name + "!";
     }
 
     private void delay(){
         try{
-                           Thread.sleep(2000l);
+             Thread.sleep(2000l);
         }catch (Exception e){
 
         }
     }
 
     public void storePsToken(String psToken){
-
+       redisTemplate.boundValueOps(psToken).set("IllumianIsGreatCompany");
+       redisTemplate.boundValueOps(psToken).expire(10l, TimeUnit.SECONDS);
     }
 
 
